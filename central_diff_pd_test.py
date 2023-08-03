1640cc18ac6d1f553ee5b135c598651198077532
Marco AcerLaptop <marcovillegas.g@gmail.com>
2023-08-03 14:52:44 -0400

    new script added

---
+++ central_diff_pd_test.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def differentiate_dataframe(df, xname, yname, order=2):
+    def central_diff(x, y):
+        # Preallocation
+        y_diff = np.empty([len(x)])
+        y_diff[:] = np.nan
+        # Computing central finite difference
+        delta_x = x[2:] - x[:-2]
+        delta_y = y[2:] - y[:-2]
+        y_diff[1 : len(x) - 1] = delta_y / delta_x  # Result definition
+        return y_diff
+
+    order_names = ["First diff", "Second diff", "Third diff"]
+    col_names = [yname] + order_names
+
+    for i in range(order):
+        # DataFrame to numpy array
+        x = df[xname].to_numpy()
+        y = df[col_names[i]].to_numpy()
+
+        df[col_names[i + 1]] = central_diff(x, y)
+        df.dropna(inplace=True)
+
+    return df
+
+
+# Reading .csv file with chromatogram data
+df1 = pd.read_csv(r"chrom-test1.csv", sep=";", decimal=",")
+df = pd.read_csv(r"chrom-test1.csv", sep=";", decimal=",")
+df["Int SMA30"] = df["Intensity"].rolling(15).mean()
+print("=" * 50 + "\n", df)
+df.dropna(inplace=True)
+print("=" * 50 + "\n", df)
+
+
+df = differentiate_dataframe(df, "min", "Int SMA30", order=2)
+
+print("=" * 50 + "\n", df)
+
+# Find the peak in range
+diff2_sorted = df[df["min"].between(0, 8)].sort_values("Second diff")
+chroma_sorted = df[df["min"].between(0, 8)].sort_values("Int SMA30")
+print(diff2_sorted.head())
+
+print(chroma_sorted.tail())
+
+
+fig, ax = plt.subplots(figsize=(10, 5))
+ax.plot(df["min"], df["Int SMA30"], label="Chromatogram")
+ax.plot(df["min"], df["First diff"], label="first derivative")
+ax.plot(diff2_sorted.iloc[0:5, 0], diff2_sorted.iloc[0:5, 2], marker=".")
+# ax.plot(chroma_sorted.iloc[-1, 0], chroma_sorted.iloc[-1, 2], marker=".")
+# ax.vlines(new_df.iloc[0, 0], -1e5, 1e5, colors="g", linestyles="dashed")
+ax.legend()
+plt.show()
--- peak_integration.py
+++ peak_integration.py
@@ -1,6 +1,24 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
+def central_diff(x, y):
+    # This function computes the central finite difference for multiple experimental
+    # points (x_i,y_i) defined as array.
+    # Inputs:
+    #   x: array of x values (n elements)
+    #   y: array of y values (n elements)}
+    # Output:
+    #   x_diff: array of x values (n-2 elements)
+    #   y_diff: array of first derivative (n-2 elements)
+    delta_x = x[2:] - x[:-2]
+    delta_y = y[2:] - y[:-2]
+    y_diff = delta_y / delta_x
+    x_diff = x[1 : len(x) - 1]
+
+    return x_diff, y_diff
+
+
 # Reading .csv file with chromatogram data
 df1 = pd.read_csv(r"chrom-test1.csv", sep=";", decimal=",")
 df = pd.read_csv(r"chrom-test1.csv", sep=";", decimal=",")
@@ -9,24 +27,22 @@ print("=" * 50 + "\n", df)
 df.dropna(inplace=True)
 print("=" * 50 + "\n", df)
 
-df[["Int SMA30", "Intensity"]].plot(figsize=(16, 8))
+# df[["Int SMA30", "Intensity"]].plot(figsize=(16, 8))
 
+interval = [5, 8]
 
 # Data frame to numpy array
 x = df["min"].to_numpy()
 y = df["Int SMA30"].to_numpy()
-print("---" * 8)
-# stepforward finite difference
-delta_x = x[1:] - x[:-1]  # forward step
-delta_y = y[1:] - y[:-1]
 
-diff = delta_y / delta_x
+print("---" * 8)
+
+x_diff, y_diff = central_diff(x, y)
 
 fig, ax = plt.subplots(figsize=(10, 5))
-ax.plot(x[1:], diff, label="260 nm")
-ax.plot(x, y)
+ax.plot(x_diff, y_diff, label="first derivative")
+# ax.plot(x_2diff, y_2diff, label="second derivative")
+ax.plot(x, y, label="Chromatogram")
+ax.vlines([5, 8], -1e5, 1e5, colors="g", linestyles="dashed")
+ax.legend()
 plt.show()
-
-print(diff)
-print(len(diff))
-print(len(x))
