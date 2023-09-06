cdc5a2af8e188124285c181ac0c88c3cd6adfeff
Marco Asus Laptop <marcovillegas.g@gmail.com>
2023-09-06 14:52:10 -0300

    Plot option removed. Now con be imported from other scripts

--- merge_channels.py
+++ merge_channels.py
@@ -6,116 +6,101 @@ each channel in the same chromatogram."""
 
 
 import os
+import sys
 import matplotlib.pyplot as plt
 import pandas as pd
 
 
-def chroma_plot(chroma_df, save_path="None", xlim="None", ylim="None", title="None"):
-    # Creating figure,ax
-    ax = chroma_df.plot(figsize=(17, 7))
+def merge_data(origin_path, channels, chroma_name, save_path="None"):
+    """This function take the data of multiple channels of one chromatogram
+    and merge them in one dataframe. Then the dataFrame is saved in with the same
+    chromatogram name but in a different folder.
 
-    # Set limits
-    if ylim != "None":
-        ax.set_ylim(ylim[0], ylim[1])
-    if xlim != "None":
-        ax.set_xlim(xlim[0], xlim[1])
-    # Set title
-    if title != "None":
-        ax.set_title(title)
+    Inputs:
+       data_paths: dictionary that allocate the directories paths were
+       chromatogram data is stored
+       file_name: name of chromatogram csv file (ex. "chromatograme.csv))
 
-    ax.set_ylabel("Intensity")  # Set labels
-    plt.grid()  # Turn grid on
-
-    # Show or save image
-    if save_path == "None":
-        plt.show()
-    else:
-        print("\tSaving in plot in:")
-        print("\t\t" + save_path)
-        plt.savefig(save_path, dpi=300)
-
-    plt.close()
-
-
-def merge_data(data_paths, file_name, save_path="None"):
-    # Inputs:
-    #   data_paths: dictionary that allocate the directories paths were
-    #   chromatogram data is stored
-    #   file_name: name of chromatogram csv file (ex. "chromatograme.csv)"
-    # Output:
-    #   chroma_data: data frame that contains multiple channels data
+     Output:
+       chroma_data: data frame that contains multiple channels data"""
 
     # Preallocation
-    chroma_data = pd.DataFrame()
-    # Dictionary loop
-    for key in data_paths:
-        print("\tReading", key, "data")
-        # Get directory path to be read
-        read_path = data_paths[key]
-        read_path = read_path + "\{}"
-        # Chromatogram path to be read
-        chroma_path = read_path.format(file_name)
+    chroma_data = pd.DataFrame(columns=channels)
+
+    # channels loop
+    for channel in channels:
+        print("\tReading", channel, "data")
+        read_path = os.path.join(origin_path, channel, chroma_name + ".csv")
+
         # Read .csv file (time variable is omitted)
-        chroma_data[key] = pd.read_csv(
-            chroma_path, sep=";", decimal=",", usecols=["Intensity"]
+        chroma_data[channel] = pd.read_csv(
+            read_path, sep=";", decimal=",", usecols=["Intensity"]
         )
 
     if save_path != "None":
         print("\tSaving chromatogram dataframe in:")
-        save_path = save_path + "\{}"
-        print("\t\t" + save_path.format(file_name))
+        print("\t\t" + os.path.join(save_path, chroma_name + ".csv"))
         chroma_data.to_csv(
-            save_path.format(file_name),
+            os.path.join(save_path, chroma_name + ".csv"),
             sep=";",
             header=True,
             index=False,
         )
+
     return chroma_data
 
 
-def main(data_paths, output_data_path, output_image_path="None"):
-    # Input:
-    #    data_paths: dictionary that allocate the directories paths were
-    #    output_data_path: directory to store arranged data
-    #    output_image_path: directory to store chromatograms images (optional)
-
+def main(main_path):
     print("The program has been started".center(79, "="))
+    print(sys.argv)
+    ## Standard folder names. Can't be changed
+    origin_folder = "original_data"
+    out_data_folder = "merged_data"
+    out_image_folder = "chromatogram_plot"
+
+    origin_path = os.path.join(main_path, origin_folder)
+
+    out_data_path = os.path.join(main_path, out_data_folder)
+
+    out_image_path = os.path.join(main_path, out_image_folder)
+
+    os.mkdir(out_data_path)  # Create output data directory
+    # os.mkdir(out_image_path)  # Create output images directory
+
+    print(os.path.join(main_path, origin_folder))
+
     # When you usen multichannels (or wavelenghts) in chromatography, each chanell
     # has de same number of data points
-    channels = list(dic_pda.keys())
-    dir_list = os.listdir(dic_pda[channels[0]])
+    channels = os.listdir(origin_path)
+    print(channels)
 
-    counter = "({}/" + str(len(dir_list)) + ")"
+    # List of chromatogram names
+    chroma_list = os.listdir(os.path.join(origin_path, channels[0]))
+    print(chroma_list)
+
+    # Counter
+    counter = "({}/" + str(len(chroma_list)) + ")"
     i = 0
+
     # Loop of chromatograms
-    for file_name in dir_list:
+    for chroma_name in chroma_list:
         # Counter
         i = i + 1
+
         # Get chromatogram name without extention
-        chroma_name = file_name.split(".")
+        chroma_name = chroma_name.split(".")
         chroma_name = chroma_name[0]
-        print(counter.format(str(i)), "Processing chromatogram", chroma_name)
+        print(counter.format(str(i)), "Merging data from chromatogram", chroma_name)
 
         # Merge multichannel data
-        chroma_data = merge_data(dic_pda, file_name, save_path=output_data_path)
+        chroma_data = merge_data(
+            origin_path, channels, chroma_name, save_path=out_data_path
+        )
 
-        # Plot multichannel chromatograms (Optional)
-        if output_image_path != "None":
-            image_path = output_image_path + chroma_name + ".png"
-            chroma_plot(chroma_data, save_path=image_path, title=file_name)
+        # if image == "True":
+        #    image_path = os.path.join(out_image_path, chroma_name + ".png")
+        #    channel_plot(chroma_data, show_time="True", save_path=image_path)
 
 
-# INPUTS
-dic_pda = {
-    "215nm": r"C:\Users\marco\Escritorio\chroma_plot\test_data\chrom_test_215nm",
-    "260nm": r"C:\Users\marco\Escritorio\chroma_plot\test_data\chrom_test_260nm",
-}
-
-output_data_path = r"C:\Users\marco\Escritorio\chroma_plot\test_data\joined_data"
-output_image_path = r"C:\Users\marco\Escritorio\chroma_plot\test_data\results_images"
-
-# MAIN CODE
-main(
-    dic_pda,
-    output_data_path,
-)
+if __name__ == "__main__":
+    main(sys.argv[1])
