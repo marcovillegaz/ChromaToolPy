dd1779e8289faedcbb8cbdefab1d017a395552d2
Marco Asus Laptop <marcovillegas.g@gmail.com>
2023-09-14 14:36:22 -0300

    Now the program could be executed in command line interface

--- chroma_rename.py
+++ chroma_rename.py
@@ -9,55 +9,108 @@ example:
     MV__21072023 M0_013 (10).csv    has been renames as      M0_03.csv
 
 """
-# This program need to be rewrite defining functions
 
 import os
 import sys
+import argparse
 import pandas as pd
 
 
 # ==============================================================================
 def main(path, index_number, new_tag):
+    """
+    path: folder path where crhomatograms are stored as csv files
+    new_tag: new tag for sample group
+    index: postion index of the identification number of individual samples
+    """
+    print("input arguments".center(85, "="))
+    print("FOLDERPATH: ", path)
+    print("NEWTAG: ", new_tag)
+    print("INDEX: ", index_number, "\n")
+
     dir_list = os.listdir(path)  # name of files in directory
 
+    flag_name = " "  # This is for counting duplicated samples
     # Extract sample name
     #   example: MV__21072023 M0_013 (10).csv
     #                         ^^^^^^
-    sample_names = [None] * len(dir_list)
-    flag_name = " "  # This is for counting duplicated samples
     for i, file_name in enumerate(dir_list):
-        print(file_name)
+        # Generating new name
         words = file_name.split()
-        sample_name = words[1]
-        # print(sample_name)
+        old_chroma_name = words[1]
+        old_chroma_name = old_chroma_name[:-4]
+        new_chroma_name = new_tag + old_chroma_name[index_number:]
 
-        new_sample_name = new_tag + sample_name[index_number]
-
-        # Count duplicates
-        if new_sample_name == flag_name:
+        # Count replicates
+        if new_chroma_name == flag_name:
             j = j + 1
         else:
             j = 1
 
-        flag_name = new_sample_name
+        flag_name = new_chroma_name
+        # Add replucates to name
+        new_chroma_name = (
+            new_chroma_name + " (" + str(j) + ").csv"
+        )  # change this to formatedd string in the forure
 
-        new_sample_name = new_sample_name + " (" + str(j) + ").csv"
-        print(new_sample_name)
+        # New name verification
+        print(file_name, "\thas been renames as\t", new_chroma_name)
+        if i == 0:
+            verification = input("The name changing is correct? Write 'yes' or 'not'.")
+            if verification == "not":
+                print("Program stopped. Verify your input argument")
+                break
+            elif verification == "yes":
+                print("The program shall continue :)")
+            else:
+                print("Not a valid response. Program stopped")
+                break
 
-    """  # Loop for changing name in specified order
-    loop_path = path + "\{}"
-    for i in range(0, n):
-        # Creating old_name and new_name paths
-        new_name_path = loop_path.format(new_names[i])
-        old_name_path = loop_path.format(old_names[i])
-        # Rename chromatograms files
+        # Loop for changing name in specified order
+        new_name_path = os.path.join(path, new_chroma_name)
+        old_name_path = os.path.join(path, dir_list[i])
+
+        # Rename files
         os.rename(old_name_path, new_name_path)
-        print(old_names[i], "\thas been renames as\t", new_names[i]) """
 
 
 if __name__ == "__main__":
-    print(sys.argv)
-    path = sys.argv[1]
-    index = int(sys.argv[2])
-    new_tag = sys.argv[3]
-    main(path, index, new_tag)
+    # Defining description adn epilogue of the program
+    parser = argparse.ArgumentParser(
+        description="Change the name of multiple chromatograms stored in one folder and identify replicates.\n ",
+        epilog="""Example: The old chromatogram is named 'MV__21072023 M0_011 (10).csv' \n
+        MV__21072023\tis the sequence name (depends of the adquicition data software).\n
+        (M)0_011\tM is the is the old sample group name. All the files in FOLDERPATH are identified with the same letter.
+        \t\tThis latter will be replace with NEWTAG as M0 -> NEWTAG0
+        M(0)_011\tis the sample number. Each sample, and its replecates, are identified with an specific number. 
+        \t\tThis number will remain intact.
+
+        New chromatogram name will be NEWTAG0_01  (replicate 1 of sample 0 from group NEWTAG)""",
+        formatter_class=argparse.RawTextHelpFormatter,
+    )
+    # Defining argument to use the program in Console Line Interface
+    parser.add_argument(
+        "-fp",
+        "--folderpath",
+        type=str,
+        required=True,
+        help="the path of the folder that contains chromatograms",
+    )
+    parser.add_argument(
+        "-ntg",
+        "--newtag",
+        type=str,
+        required=True,
+        help="New tag for rename your chromatograms",
+    )
+    parser.add_argument(
+        "-idx",
+        "--index",
+        type=int,
+        required=True,
+        help="Position where of the sample number. In the example M'0'_011 zero has index 1",
+    )
+
+    args = parser.parse_args()
+
+    main(path=args.folderpath, new_tag=args.newtag, index_number=args.index)
