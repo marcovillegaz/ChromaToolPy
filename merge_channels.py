f7caacb6823b841bce08365135cba48d430ba8b5
Marco Asus Laptop <marcovillegas.g@gmail.com>
2023-09-14 15:20:30 -0300

    Now can be called in Command Line Interface

--- merge_channels.py
+++ merge_channels.py
@@ -6,9 +6,9 @@ each channel in the same chromatogram."""
 
 
 import os
-import sys
 import matplotlib.pyplot as plt
 import pandas as pd
+import argparse
 
 
 def merge_data(origin_path, channels, chroma_name, save_path="None"):
@@ -52,29 +52,25 @@ def merge_data(origin_path, channels, chroma_name, save_path="None"):
 
 def main(main_path):
     print("The program has been started".center(79, "="))
-    print(sys.argv[:])
-    ## Standard folder names. Can't be changed
-    origin_folder = "original_data"
-    out_data_folder = "merged_data"
-    out_image_folder = "images"
 
-    origin_path = os.path.join(main_path, origin_folder)
+    # ADD A FUNCTION THAT RECOGNIZE IF THE FOLDERS ARE CREATED OR NOT
 
-    out_data_path = os.path.join(main_path, out_data_folder)
-
-    out_image_path = os.path.join(main_path, out_image_folder)
+    ## create folder structure. Can't be changed
+    origin_path = os.path.join(main_path, "original_data")
+    out_data_path = os.path.join(main_path, "merged_data")
+    # out_image_path = os.path.join(main_path, "images")
 
     os.mkdir(out_data_path)  # Create output data directory
-    os.mkdir(out_image_path)  # Create output images directory
+    # os.mkdir(out_image_path)  # Create output images directory
 
-    print(os.path.join(main_path, origin_folder))
+    print(origin_path)
 
-    # When you usen multichannels (or wavelenghts) in chromatography, each chanell
+    # When you use photodiode array in chromatography, each chanell
     # has de same number of data points
-    channels = os.listdir(origin_path)
+    channels = os.listdir(origin_path)  # Get the channels names
     print(channels)
 
-    # List of chromatogram names
+    # List of chromatogram names in first channel
     chroma_list = os.listdir(os.path.join(origin_path, channels[0]))
     print(chroma_list)
 
@@ -102,5 +98,27 @@ def main(main_path):
         #    channel_plot(chroma_data, show_time="True", save_path=image_path)
 
 
-# if __name__ == "__main__":
-# main(sys.argv)
+if __name__ == "__main__":
+    # Defining description adn epilogue of the program
+    parser = argparse.ArgumentParser(
+        description="Merged mutiple channel chromatogram into the same csv file for a group of chromatograms.",
+        epilog=r"""The MAINPATH must contain a folder named \original_data which has subfolder identified with the wavelength of the 
+        corresponding channel. For example: \original_data\260nm and  \original_data\215nm. Each channel folder has the same number 
+        of chromatograms files with the same names, but the signal is different. Then, the signals of the channel are merged into a 
+        single csv file stored in the folder \merged_data which is going to be used in further data processing.""",
+        formatter_class=argparse.RawTextHelpFormatter,
+    )
+
+    # Defining argument to use the program in Console Line Interface
+    parser.add_argument(
+        "-mp",
+        "--mainpath",
+        type=str,
+        required=True,
+        help="the path where the program shall run",
+    )
+
+    args = parser.parse_args()
+
+    # main() function
+    main(main_path=args.mainpath)
