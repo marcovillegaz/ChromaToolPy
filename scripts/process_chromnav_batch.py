0632ff99f2a399487a674a070d1c1d4727cf95e9
Marco Asus Laptop <marcovillegas.g@gmail.com>
2025-06-16 18:24:38 -0400

    functional scripts

---
+++ scripts/process_chromnav_batch.py
@@ -0,0 +1,28 @@
+"""Process a single batch of data."""
+
+import os
+import shutil
+
+from src.post_processing.merged import *
+from src.post_processing.data_base import *
+
+
+# Important information to be extracted, columns
+extract_cols = ["Acquisition Date", "Chromatogram Name", "CH", "Peak Name", "Area"]
+
+raw_data_folder = r"C:\Users\marco\OneDrive - usach.cl\GWR Analysis\2_Analysis\Thais_Vargas\Raw\batch1_02"
+database_path = r"C:\Users\marco\OneDrive - usach.cl\GWR Analysis\2_Analysis\Thais_Vargas\TV_results_database.xlsx"
+
+# Merge peak information into a single dataframe
+appended_df = merge_files(raw_data_folder, extract_cols)
+
+print(appended_df)
+
+# Load information into the database workbook
+load_data(
+    new_df=appended_df,
+    workbook_path=database_path,
+    sheet_name="TABLE",
+    table_name="DataTable",
+    batch_name="Batch1_02",
+)
