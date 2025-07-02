448c69d0c4801a67d1d086f47d80dc9bf11f491c
Marco Asus Laptop <marcovillegas.g@gmail.com>
2025-07-02 17:16:59 -0400

    extended to FL chromatograms

--- scripts/display/generate_pda_plot_batch.py
+++ scripts/display/generate_pda_plot_batch.py
@@ -3,42 +3,68 @@ This script creates surface plots from PDA data for J. Vidal.
 It processes all PDA files in a given folder.
 """
 
-import os
 from src.classes.chromatogram import Chromatogram
 from src.display import chroma_plot
+import os
 
 # Input and output folders
-folder_path = r"C:\Users\marco\OneDrive - usach.cl\GWR Analysis\2_Analysis\Thais_Vargas\Raw\batch1_pda\PDA"
-output_folder = (
-    r"C:\Users\marco\OneDrive - usach.cl\GWR Analysis\2_Analysis\Thais_Vargas\Imagenes"
-)
+RAW_PDA_FOLDER_PATH = r"C:\Users\marco\OneDrive - usach.cl\GWR Analysis\2_Analysis\Thais_Vargas\Raw\batch1_03_pda\PDA"
+RAW_FL_FOLDER_PATH = r"C:\Users\marco\OneDrive - usach.cl\GWR Analysis\2_Analysis\Thais_Vargas\Raw\batch1_03_pda\FL"
+PDA_OUTPUT_FOLDER = r"C:\Users\marco\OneDrive - usach.cl\GWR Analysis\2_Analysis\Thais_Vargas\Imagenes\batch1_03\PDA"
+FL_OUTPUT_FOLDER = r"C:\Users\marco\OneDrive - usach.cl\GWR Analysis\2_Analysis\Thais_Vargas\Imagenes\batch1_03\FL"
 
 # Ensure output directory exists
-if not os.path.exists(output_folder):
-    os.makedirs(output_folder)
+if not os.path.exists(PDA_OUTPUT_FOLDER):
+    os.makedirs(PDA_OUTPUT_FOLDER)
 
 # Loop through all .txt files in the input folder
-for filename in os.listdir(folder_path):
+total_files = len(os.listdir(RAW_PDA_FOLDER_PATH))
+
+for index, filename in enumerate(os.listdir(RAW_PDA_FOLDER_PATH), start=1):
+    print(f"({index}/{total_files}) Processing: {filename}")
+
     if filename.lower().endswith(".txt"):
-        file_path = os.path.join(folder_path, filename)
-        print(f"Processing: {filename}")
+        pda_file_path = os.path.join(RAW_PDA_FOLDER_PATH, filename)
 
         try:
             # Create Chromatogram instance
-            chroma = Chromatogram.create_from_pda(file_path)
-
-            print(f"Chromatogram name: {chroma.NAME}")
+            chroma = Chromatogram.create_from_pda(pda_file_path)
+            print(f"\tChromatogram name: {chroma.NAME}")
 
             # Generate and save plots
             chroma_plot.pda_contour(
-                chroma, y_max=400, z_min=-10000, z_max=530000, save_path=output_folder
+                chroma,
+                y_max=400,
+                z_min=-10000,
+                z_max=530000,
+                save_path=PDA_OUTPUT_FOLDER,
             )
-
             chroma_plot.pda_2d(
                 chroma,
                 wavelengths=[250, 300],  # Modify as needed
-                save_path=output_folder,
+                save_path=PDA_OUTPUT_FOLDER,
             )
+
+        except Exception as e:
+            print(f"Error processing {filename}: {e}")
+
+
+# Loop through all .csv files in the input FL folder
+total_files = len(os.listdir(RAW_FL_FOLDER_PATH))
+
+for index, filename in enumerate(os.listdir(RAW_FL_FOLDER_PATH), start=1):
+    print(f"({index}/{total_files}) Processing: {filename}")
+
+    if filename.lower().endswith(".csv"):
+        fl_file_path = os.path.join(RAW_FL_FOLDER_PATH, filename)
+
+        try:
+            # Create Chromatogram instance
+            chroma = Chromatogram.create_from_fl(fl_file_path)
+            print(f"\tChromatogram name: {chroma.NAME}")
+
+            # Generate and save plots
+            chroma_plot.fl_plot(chroma, FL_OUTPUT_FOLDER)
 
         except Exception as e:
             print(f"Error processing {filename}: {e}")
