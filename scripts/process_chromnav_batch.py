2704db18785a577b46d6eb510e897b3a726b0e89
Marco Asus Laptop <marcovillegas.g@gmail.com>
2025-07-02 17:16:37 -0400

    unused package deleted

--- scripts/process_chromnav_batch.py
+++ scripts/process_chromnav_batch.py
@@ -1,28 +1,24 @@
 """Process a single batch of data."""
 
-import os
-import shutil
-
-from src.io.merge_channels import *
-from src.io.update_excel_db import *
-
+from src.io.merge_channels import merge_files
+from src.io.update_excel_db import load_data
 
 # Important information to be extracted, columns
 extract_cols = ["Acquisition Date", "Chromatogram Name", "CH", "Peak Name", "Area"]
 
-raw_data_folder = r"C:\Users\marco\OneDrive - usach.cl\GWR Analysis\2_Analysis\Thais_Vargas\Raw\batch1_02"
-database_path = r"C:\Users\marco\OneDrive - usach.cl\GWR Analysis\2_Analysis\Thais_Vargas\TV_results_database.xlsx"
+RAW_DATA_FOLDER = r"C:\Users\marco\OneDrive - usach.cl\GWR Analysis\2_Analysis\Thais_Vargas\Raw\batch1_03"
+DATABASE_PATH = r"C:\Users\marco\OneDrive - usach.cl\GWR Analysis\2_Analysis\Thais_Vargas\2025_06_16 - TV_results_database - copia.xlsx"
 
 # Merge peak information into a single dataframe
-appended_df = merge_files(raw_data_folder, extract_cols)
+appended_df = merge_files(RAW_DATA_FOLDER, extract_cols)
 
 print(appended_df)
 
 # Load information into the database workbook
 load_data(
     new_df=appended_df,
-    workbook_path=database_path,
+    workbook_path=DATABASE_PATH,
     sheet_name="TABLE",
     table_name="DataTable",
-    batch_name="Batch1_02",
+    batch_name="Batch1_03",
 )
